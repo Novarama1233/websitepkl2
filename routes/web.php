fa426ff5451b64a53390415be0ae7136cd552fcb
Novarama1233 <novarama762@gmail.com>
2025-07-24 15:02:45 +0700

    pklwebsite

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'authenticated']);
+Route::get('/logout', [AuthController::class, 'logout']);
+
+Route::get('/dashboard', [DashboarDcONTROLLER::class, 'index'])->middleware('auth');

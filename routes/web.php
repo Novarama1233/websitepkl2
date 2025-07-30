64470c02fb10edae6e6a436d2f5e1b9b5e426ccb
Novarama1233 <novarama762@gmail.com>
2025-07-30 12:36:48 +0700

    pklwebsite

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,12 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [HomeController::class, 'about']);
+Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/portfolio', [HomeController::class, 'portfolio']);
+Route::get('/services', [HomeController::class, 'services']);
+Route::get('/team', [HomeController::class, 'team']);
+Route::get('/testimonials', [HomeController::class, 'testimonials']);
 
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::class, 'authenticated']);

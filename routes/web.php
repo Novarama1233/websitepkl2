0582a848ab611ccf053239796dd5bca05027b251
Novarama1233 <novarama762@gmail.com>
2025-07-31 16:40:29 +0700

    pklwebsite

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\SliderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
@@ -18,3 +19,5 @@ Route::post('/login', [AuthController::class, 'authenticated']);
 Route::get('/logout', [AuthController::class, 'logout']);
 
 Route::get('/dashboard', [DashboarDcONTROLLER::class, 'index'])->middleware('auth');
+
+Route::resource('sliders', SliderController::class)->middleware('auth');
